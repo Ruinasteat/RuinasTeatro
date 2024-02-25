c3103318400b75f7516644795202974f9a6bb605
Ruinasteat <161256444+Ruinasteat@users.noreply.github.com>
2024-02-25 22:07:17 +0000

    Add files via upload

---
+++ enviar_email.php
@@ -0,0 +1,34 @@
+<?php
+// Verifica se o formulário foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coleta os dados do formulário
+    $nome = $_POST['Nome'];
+    $email = $_POST['Email'];
+    $telefone = $_POST['Telefone'];
+    $mensagem = $_POST['Mensagem'];
+
+    // Endereço de e-mail para onde você deseja enviar a mensagem
+    $destinatario = 'teatroruinas@gmail.com';
+
+    // Assunto do e-mail
+    $assunto = 'Nova mensagem do formulário de contato';
+
+    // Monta o corpo da mensagem
+    $corpo_mensagem = "Nome: $nome\n";
+    $corpo_mensagem .= "Email: $email\n";
+    $corpo_mensagem .= "Telefone: $Telefone\n";
+    $corpo_mensagem .= "Mensagem:\n$mensagem";
+
+    // Envia o e-mail
+    if (mail($destinatario, $assunto, $corpo_mensagem)) {
+        // Se o e-mail foi enviado com sucesso, redireciona de volta para o formulário com uma mensagem de sucesso
+        header('Location: formulario_contato.html?enviado=sucesso');
+    } else {
+        // Se houve um erro no envio do e-mail, redireciona de volta para o formulário com uma mensagem de erro
+        header('Location: formulario_contato.html?enviado=erro');
+    }
+} else {
+    // Se o formulário não foi submetido, redireciona de volta para o formulário
+    header('Location: ./contact.html');
+}
+?>
